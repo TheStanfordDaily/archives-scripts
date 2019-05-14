6817bf761277be123ffcac92cf11db042dd7b7c5
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-05-14 15:05:57 +0000

    fix: new format for files etc.

--- upload-search.js
+++ upload-search.js
@@ -16,14 +16,21 @@ async function main() {
     let papers = await fetchAllPapers();
     for (let i in papers) {
         let paper = papers[i];
-        if (paper.date.getFullYear() < 1921) {
+        if (process.argv.length > 2 && !isNaN(process.argv[2]) && paper.date.getFullYear() < parseInt(process.argv[2])) {
             console.log("skipping paper", paper.date);
             continue;
         }
         let pages = await paper.getPages();
         for (let page of pages) {
             await page.getAltoData();
-            let dir = `text/${page.date.getFullYear()}/${page.date.getMonth() + 1}/${page.date.getDate()}`;
+            let month = page.date.getMonth() + 1;
+            month = (month < 10) ? "0" + month : "" + month;
+            let date = page.date.getDate();
+            date = (date < 10) ? "0" + date : "" + date;
+            let year = "" + page.date.getFullYear();
+            let yearx = year.slice(0, 3) + "x";
+            let yearxx = year.slice(0, 2) + "xx";
+            let dir = `text3/${yearxx}/${yearx}/${year}/${month}/${date}`;
             await new Promise(function(resolve, reject) {
                 fs.mkdir(dir, { recursive: true }, (err) => {
                   if (err) reject(err);
@@ -36,13 +43,13 @@ async function main() {
                 //     title: 'Young Republicans.',
                 //     sectionID: 'MODSMD_ARTICLE4',
                 //     areaIDs: [ 'P1_TB00014', 'P1_TB00015' ] },
-                let path = `${dir}/${section.sectionID}.txt`;
+                let path = `${dir}/${section.sectionID}.${section.type.toLowerCase()}.txt`;
                 if (fs.existsSync(path)) {
                     console.log("skipping", path);
                     continue;   
                 }
-                console.log(path, section.title);
-                let text = await page.getSectionText(section);
+                console.log(path, section.title, section.subtitle, section.author);
+                let text = ["# " + section.title, "## " + section.subtitle, "### " + section.author, await page.getSectionText(section)].join("\n");
                 await new Promise(function(resolve, reject) {
                     fs.writeFile(path, text, function(err) {
                         if (err) reject(err);
