230e58ea1dc558e7c555bd8bf32bebff829382f1
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-05-14 04:32:41 +0000

    upload search bitbucket

--- README.md
+++ README.md
@@ -21,3 +21,34 @@ aws lambda add-permission --profile tsd --function-name archives-resizer --princ
 sudo yum install libjpeg-turbo8-dev libfreetype6-dev zlib1g-dev \
 liblcms2-dev liblcms2-utils libtiff5-dev python-dev libwebp-dev apache2 \
 libapache2-mod-wsgi
+
+
+shopt -s globstar
+rename -n 's/special/regular/' **
+
+rename -n 's/\<[0-9]\>/0&/' **
+
+du -h text
+find text -type f | wc -l 
+
+
+"""
+Three Penalized Houses Pledge 35 New Women
+Author Author Author
+-----------
+
+File Name:
+1900-1919/1900/12/13/MODSMD_ARTICLE4.article.txt
+
+1900-1919
+1920-1929
+1930-1939
+1940-1949
+1950-1959
+
+1900-2000-1919
+
+
+1900-2000?
+
+"""
--- upload-search.js
+++ upload-search.js
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const INPUT_BUCKET = "stanforddailyarchive";
 const {fetchAllPapers} = require("@thestanforddaily/archives-web/lib/helpers/papers");
-
+const fs = require("fs");
 
 main();
 
@@ -14,5 +14,46 @@ async function main() {
     //     console.log(year);
     // }
     let papers = await fetchAllPapers();
-    return papers;
+    for (let i in papers) {
+        let paper = papers[i];
+        if (paper.date.getFullYear() < 1921) {
+            console.log("skipping paper", paper.date);
+            continue;
+        }
+        let pages = await paper.getPages();
+        for (let page of pages) {
+            await page.getAltoData();
+            let dir = `text/${page.date.getFullYear()}/${page.date.getMonth() + 1}/${page.date.getDate()}`;
+            await new Promise(function(resolve, reject) {
+                fs.mkdir(dir, { recursive: true }, (err) => {
+                  if (err) reject(err);
+                  else resolve();
+                });
+            });
+            for (let section of page.sections) {
+                //   PageSection {
+                //     type: 'ARTICLE',
+                //     title: 'Young Republicans.',
+                //     sectionID: 'MODSMD_ARTICLE4',
+                //     areaIDs: [ 'P1_TB00014', 'P1_TB00015' ] },
+                let path = `${dir}/${section.sectionID}.txt`;
+                if (fs.existsSync(path)) {
+                    console.log("skipping", path);
+                    continue;   
+                }
+                console.log(path, section.title);
+                let text = await page.getSectionText(section);
+                await new Promise(function(resolve, reject) {
+                    fs.writeFile(path, text, function(err) {
+                        if (err) reject(err);
+                        else resolve();
+                    });
+                });
+                // break;
+            }
+            // break;
+        }
+        papers[i] = null;
+        // break;
+    }
 }
