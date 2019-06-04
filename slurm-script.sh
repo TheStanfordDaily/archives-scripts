d32223bd3c60906f1e5822df3b9c3708a2a126c3
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-06-04 09:19:51 -0700

    smaller slurm jobs

--- slurm-script.sh
+++ slurm-script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=tsd_archives_text_extractor    # Job name
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --ntasks=2                    # Run on a single CPU
-#SBATCH --mem=1600mb                     # Job memory request
+#SBATCH --ntasks=1                    # Run on a single CPU
+#SBATCH --mem=800mb                     # Job memory request
 #SBATCH --time=40:00:00               # Time limit hrs:min:sec
 #SBATCH --output=slurm_logs/%A_%a.log   # Standard output and error log
 pwd; hostname; date
